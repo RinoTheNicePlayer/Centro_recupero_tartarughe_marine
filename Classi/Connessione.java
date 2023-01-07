f6a677324547c31efd0e020d68e13cf1e17d984c
antoniodim <antoniodimaso.adm1511@gmail.com>
2023-01-07 13:11:43 +0100

    tweak: la Connessione non avviene più ogni volta che si tenta l'accesso, ma solo quando si accede con successo.

--- Classi/Connessione.java
+++ Classi/Connessione.java
@@ -4,22 +4,26 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class Connessione {
-	//Funzione che permette di connettere Eclipse a PostgreSQL
+	
+	private static Connection connessione = null;
+	
+	//Funzione che permette di connettere Java a Postgres
 	public static Connection getConnection() {
-		Connection connessione = null;
 		
 		try {
 			Class.forName("org.postgresql.Driver");
 			
-			connessione = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Centro_recupero_tartarughe_marine", "postgres", "postgres");
-			
-			if(connessione != null) {
-				System.out.println("La connessione al server di PostgreSQL e' avvenuta con successo!");
+			if(connessione == null) {
+				connessione = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Centro_recupero_tartarughe_marine", "postgres", "postgres");
+				
+				if(connessione != null) {
+					System.out.println("La connessione al server di PostgreSQL e' avvenuta con successo!");
+				
+				} else {
+					System.out.println("La connessione al server di PostgreSQL e' fallita!");
+				}
 			}
 			
-			else {
-				System.out.println("La connessione al server di PostgreSQL e' fallita!");
-			}
 		} catch(Exception e) {
 			System.out.println("" + e);
 		}
